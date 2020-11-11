dccb286475bf6090504406e0163e7281a1ec0349
Vishal Singh Kushwaha <vishal.02jan@gmail.com>
2020-11-11 18:39:41 +0530

    Update setup.py

--- allure-pytest/setup.py
+++ allure-pytest/setup.py
@@ -32,15 +32,16 @@ setup_requires = [
 install_requires = [
     "pytest>=4.5.0",
     "six>=1.9.0",
+    "allure-python-commons==2.8.*",
 ]
 
 
-def prepare_version():
-    from setuptools_scm import get_version
-    configuration = {"root": "..",  "relative_to": __file__}
-    version = get_version(**configuration)
-    install_requires.append("allure-python-commons==2.8.*")
-    return configuration
+# def prepare_version():
+#     from setuptools_scm import get_version
+#     configuration = {"root": "..",  "relative_to": __file__}
+#     version = get_version(**configuration)
+#     install_requires.append("allure-python-commons==2.8.*")
+#     return configuration
 
 
 def get_readme(fname):
@@ -50,7 +51,7 @@ def get_readme(fname):
 def main():
     setup(
         name=PACKAGE,
-        use_scm_version=prepare_version,
+        use_scm_version={"root": "..",  "relative_to": __file__},
         description="Allure pytest integration",
         url="https://github.com/allure-framework/allure-python",
         author="QAMetaSoftware, Stanislav Seliverstov",
